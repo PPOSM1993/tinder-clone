c87a8f032db4de77298706ec145d237102bec9a2
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-11-24 16:57:15 -0300

    Se trabaja en el sitio de perfil de usuario

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <nav className="relative z-50 bg-slate-900 border-b border-gray-200/50 dark:border-gray-700/50">
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between h-16">
-          
+
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-3">
             <span className="text-xl font-bold bg-gradient-to-r from-gray-900 to-gray-700 dark:from-white dark:to-gray-200 bg-clip-text text-transparent">
---
+++ lib/actions/profile.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { createClient } from "../supabase/server"
+
+
+export async function getCurrentUserProfile() {
+    const supabase = await createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+        return null
+    }
+
+    const { data: profile, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+
+    if (error) {
+        console.log("Error fetching profile", error)
+        return null
+    }
+
+    return profile
+
+}
