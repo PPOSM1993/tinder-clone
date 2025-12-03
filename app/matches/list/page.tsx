165f8179a6d0a35adf9afc002da445ff7fb303c3
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-12-03 17:06:47 -0300

    Se trabaja en el envio de match a otros usuarios

--- app/matches/list/page.tsx
+++ app/matches/list/page.tsx
@@ -1,12 +1,9 @@
-"use client"
+"use client";
+
 export default function MatchesListPage() {
-    return(
+    return (
         <>
-        <div className="min-h-screen bg-gradient-to-br from-pink-50 to-red-50 dark:from-gray-900 dark:to-gray-800 flex items-center justify-center">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                Tinder Clone xd
-                </h1>
-        </div>
+            xD
         </>
     )
 }
